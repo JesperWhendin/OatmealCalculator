0c55a854f8e974d5d5aca92dcb3f7a6d89824250
JesperWhendin <143404976+JesperWhendin@users.noreply.github.com>
2024-03-18 17:05:51 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OatmealCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
